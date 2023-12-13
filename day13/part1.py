6c4352605d346b31af05fbe7c1dd249a7dd7b5d2
sndb <sndb@sndb.xyz>
2023-12-14 02:49:21 +0300

    day 13: refactor

--- day13/part1.py
+++ day13/part1.py
@@ -1,24 +1,19 @@
-from itertools import groupby
-
-
 def ref(l):
     for i in range(1, len(l)):
         d = min(i, len(l) - i)
         a = l[i - d : i]
-        b = l[i : i + d]
-        if a == list(reversed(b)):
+        b = l[i : i + d][::-1]
+        if a == b:
             return i
     return 0
-
 
 def solve(l):
     return 100 * ref(l) + ref(list(zip(*l)))
 
-
 c = 0
 
-for k, g in groupby(open(0).read().splitlines(), bool):
-    if k:
-        c += solve(list(map(list, g)))
+for s in open(0).read().split("\n\n"):
+    g = list(map(list, s.splitlines()))
+    c += solve(g)
 
 print(c)
--- day13/part2.py
+++ day13/part2.py
@@ -1,16 +1,12 @@
-from itertools import groupby
-
-
 def ref(l):
     r = []
     for i in range(1, len(l)):
         d = min(i, len(l) - i)
         a = l[i - d : i]
-        b = l[i : i + d]
-        if a == list(reversed(b)):
+        b = l[i : i + d][::-1]
+        if a == b:
             r.append(i)
     return r
-
 
 def solve(l):
     h = ref(l)
@@ -27,11 +23,10 @@ def solve(l):
                     return i
             l[r][c] = x
 
-
 c = 0
 
-for k, g in groupby(open(0).read().splitlines(), bool):
-    if k:
-        c += solve(list(map(list, g)))
+for s in open(0).read().split("\n\n"):
+    g = list(map(list, s.splitlines()))
+    c += solve(g)
 
 print(c)
